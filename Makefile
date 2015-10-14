7708ffc12ee70f00fae9b351a1722bdfba96cb0f
AlexHung780312 <alex780312+github@gmail.com>
2015-10-15 00:29:42 +0800

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CXX = g++
 CXXFLAGS = -O3 -march=native
+#CXXFLAGS += -DSHOW_IMG_WINDOW
 LIBS = -lm -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_highgui
 all:hw1
 hw1:hw1.o
--- hw1.cpp
+++ hw1.cpp
@@ -1,8 +1,23 @@
+// author: Alex Hung
 #include <opencv/cv.hpp>
 #include <iostream>
 #include <exception>
 using namespace cv;
 using namespace std;
+void bgr2gray(Mat& src, Mat& tgt) {
+  /// Convert RGB to Grey-level
+  /// I = (R+G+B)/3
+  assert(src.rows == tgt.rows);
+  assert(src.cols == tgt.cols);
+  for (int i = 0; i < src.rows; i++) {    // for each rows
+    for (int j = 0; j < src.cols; j++) {  // for each cols
+      tgt.at<uchar>(j, i) =
+          (src.at<Vec3b>(i, j)[0] + src.at<cv::Vec3b>(i, j)[1] +
+           src.at<cv::Vec3b>(i, j)[2]) /
+          3;
+    }
+  }
+}
 int main(int argc, char* argv[]) {
   if (argc < 3) {
     cout << "usage: ./hw1 input-img output-color-img output-greylevel-img\n"
@@ -13,14 +28,17 @@ int main(int argc, char* argv[]) {
   try {
     Mat img = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     imwrite(argv[2], img);
-    Mat gray_img;
-    cvtColor(img, gray_img, CV_BGR2GRAY);
-    imwrite(argv[3], gray_img);
+    Mat grey_img(img.cols, img.rows, CV_8UC1);
+    ///// cvtColor(img, grey_img, CV_BGR2GRAY);
+    bgr2gray(img, grey_img);
+    imwrite(argv[3], grey_img);
+#ifdef SHOW_IMG_WINDOW
     namedWindow("Input image", CV_WINDOW_AUTOSIZE);
     namedWindow("Gray image", CV_WINDOW_AUTOSIZE);
     imshow("Input image", img);
-    imshow("Gray image", gray_img);
+    imshow("Gray image", grey_img);
     waitKey(0);
+#endif
   } catch (exception e) {
     cerr << "exception caught: " << e.what() << '\n';
   }
